a2f77e792a9194022c621c977a3f77d5c063b3aa
Assam157 <asasm8106@gmail.com>
2025-01-22 21:37:33 +0530

    Update ProductDeleteForm.jsx

--- src/components/ProductDeleteForm.jsx
+++ src/components/ProductDeleteForm.jsx
@@ -27,7 +27,7 @@ const ProductDelete = () => {
 
         try {
             // Make sure to use DELETE method here
-            const response = await fetch("https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/delete", {
+            const response = await fetch("https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/delete", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
