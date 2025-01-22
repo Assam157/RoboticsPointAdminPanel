9379b223c39f88ef74bcce75771a3eaf04a96bad
Assam157 <asasm8106@gmail.com>
2025-01-22 21:37:19 +0530

    Update ProductModifyForm.jsx

--- src/components/ProductModifyForm.jsx
+++ src/components/ProductModifyForm.jsx
@@ -29,7 +29,7 @@ const ProductModify = () => {
 
         try {
             // Make the POST request to modify the product
-            const response = await fetch('https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/api/products/modify', {
+            const response = await fetch('https://roboticspointbackend-b6b7b2e85bbf.herokuapp.com/api/products/modify', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
